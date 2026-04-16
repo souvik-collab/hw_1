42b10236043d3d5e9fede31c20fc6c15ea619048
souvik-collab <souvik.learn@gmail.com>
2026-04-16 23:28:41 +0530

    Add files via upload

---
+++ integration13.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include<fstream>
+using namespace std;
+
+double f(double x) {
+    return  sin(M_PI*pow(x,2)/2.0);
+}
+
+// Simpson 1/3 Rule implementation
+double simp13(double a, double b, long n) {
+    double h = (b - a) / double(n); // Width of each sub-interval
+    double sum =  f(a)+f(b); // Sum of first and last term
+
+    // Sum of middle terms multiplied by 2
+    for (int i = 1; i < n; i++) {
+        if (i%2==0)
+        {
+            sum += 2 * f(a + i * h);
+        }
+        else
+        {
+            sum += 4 * f(a + i * h);
+        }
+    }
+
+    return (h / 3) * sum;
+}
+double precision (double a ,double b)
+{
+     long intervals=10; 
+    
+
+    double i1= simp13(a, b, intervals);
+    double i2= 1e4;
+    while(abs(i2-i1)>1e-6)
+    {
+        i1=i2;
+        intervals *= 10;
+        i2= simp13(a, b, intervals);
+        
+    }
+    return i2;
+}
+int main(void)
+{
+    float i,a=0,b=5,h=(b-a)/50.0;
+    ofstream out("fresnel1.dat");
+    for(i=a;i<=b;i=i+h)
+    {
+        out<<i<<"\t"<<precision(0,i)<<endl;
+    }
+   
+}
